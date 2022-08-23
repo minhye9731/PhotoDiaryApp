39fa1af5b105e7d6837a4024969065394d32bdff
minhye9731 <jenney62639731@gmail.com>
2022-08-23 20:36:11 +0900

    Initial Commit

---
+++ PhotoDiaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoDiaryApp
+//
+//  Created by 강민혜 on 8/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
