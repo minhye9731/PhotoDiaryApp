47ae2e51238155b4d63140ecf93e155b23a92eef
minhye9731 <jenney62639731@gmail.com>
2022-08-26 10:19:16 +0900

    UserDiaryRepository 클래스 생성

---
+++ PhotoDiaryApp/Realm/UserDiaryRepository.swift
@@ -0,0 +1,16 @@
+//
+//  UserDiaryRepository.swift
+//  PhotoDiaryApp
+//
+//  Created by 강민혜 on 8/26/22.
+//
+
+import Foundation
+import RealmSwift
+
+class UserDiaryRepository {
+    
+    let localRealm = try! Realm()
+    
+    
+}
